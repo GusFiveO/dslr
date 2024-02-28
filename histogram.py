996a8f64ea34bb57a6b40f4936c1a53659e90ecb
Augustin LORAIN <alorain@bess-f2r1s12.clusters.42paris.fr>
2024-02-28 20:43:34 +0100

    the most homogeanous is determined

--- histogram.py
+++ histogram.py
@@ -7,24 +7,19 @@ import numpy as np
 import sys
 from describe import apply_mean, apply_std, describe
 
-from utils import load_csv, print_histogram_usage
+from utils import load_csv, load_pandas_csv, print_histogram_usage
 
 matplotlib.use("TkAgg")
 
 
-def z_score_normalize(column: pl.Series):
-    normalized_array = (column - apply_mean(column)) / apply_std(column)
-    return normalized_array
-
-
-def plot_histogram(df: pl.DataFrame):
+def plot_score_distribution(df):
     ROW = 4
     COLUMN = 4
     fig, axs = plt.subplots(ROW, COLUMN, layout="constrained")
-    slytherin_df = df.filter(pl.col("Hogwarts House") == "Slytherin")
-    gryffindor_df = df.filter(pl.col("Hogwarts House") == "Gryffindor")
-    ravenclaw_df = df.filter(pl.col("Hogwarts House") == "Ravenclaw")
-    hufflepuff_df = df.filter(pl.col("Hogwarts House") == "Hufflepuff")
+    slytherin_df = df[df["Hogwarts House"] == "Slytherin"]
+    gryffindor_df = df[df["Hogwarts House"] == "Gryffindor"]
+    ravenclaw_df = df[df["Hogwarts House"] == "Ravenclaw"]
+    hufflepuff_df = df[df["Hogwarts House"] == "Hufflepuff"]
     course_name_list = [
         "Arithmancy",
         "Astronomy",
@@ -57,6 +52,53 @@ def plot_histogram(df: pl.DataFrame):
         axs[col, row].axis("off")
 
     plt.show()
+
+
+def z_score_normalize(column):
+    mean = apply_mean(column)
+    std = apply_std(column)
+    normalized_column = (column - mean) / std
+    return normalized_column
+
+
+def plot_homogeneous_distribution_course(df: pl.DataFrame):
+
+    course_name_list = [
+        "Arithmancy",
+        "Astronomy",
+        "Herbology",
+        "Defense Against the Dark Arts",
+        "Divination",
+        "Muggle Studies",
+        "Ancient Runes",
+        "History of Magic",
+        "Transfiguration",
+        "Potions",
+        "Care of Magical Creatures",
+        "Charms",
+        "Flying",
+    ]
+    course_columns = df[course_name_list]
+    normalized_df = course_columns.apply(z_score_normalize)
+    normalized_df["Hogwarts House"] = df["Hogwarts House"]
+
+    print(normalized_df.groupby("Hogwarts House").apply(lambda x: x.apply(apply_mean)))
+    normalized_df = normalized_df.groupby("Hogwarts House").apply(
+        lambda x: x.apply(apply_mean)
+    )
+
+    print(normalized_df)
+    min_std = None
+    best = None
+    dict_best = dict()
+    for column in normalized_df:
+        tmp_std = apply_std(normalized_df[column])
+        dict_best[column] = tmp_std
+        if min_std is None or tmp_std < min_std:
+            min_std = tmp_std
+            best = column
+    print(best)
+    print(dict_best)
     return None
 
 
@@ -66,7 +108,9 @@ if __name__ == "__main__":
     except Exception:
         print_histogram_usage()
         exit()
-    df = load_csv(dataset_pathname)
+    df = load_pandas_csv(dataset_pathname)
     if df is None:
         exit()
-    plot_histogram(df)
+    df = df.dropna()
+    plot_score_distribution(df)
+    plot_homogeneous_distribution_course(df)
