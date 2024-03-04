6fc9f2ef419b0b9f1f3eadf6f7eb466fbf8205d5
Augustin LORAIN <alorain@bess-f2r3s17.clusters.42paris.fr>
2024-03-04 14:27:34 +0100

    feat(predict/train): removed the column Astronomy

--- logreg_train.py
+++ logreg_train.py
@@ -7,6 +7,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import argparse
 
+
 def print_usage():
     print("Usage: ./logreg_train.py dataset_pathname")
 
@@ -14,17 +15,20 @@ def print_usage():
 def sigmoid(z):
     return 1 / (1 + np.exp(-z))
 
+
 def compute_cost(X, y, weights):
     m = len(y)
     h = sigmoid(np.dot(X, weights))
     epsilon = 1e-5  # Pour éviter log(0)
-    cost = (-1/m) * np.sum(y * np.log(h + epsilon) + (1 - y) * np.log(1 - h + epsilon))
+    cost = (-1 / m) * np.sum(
+        y * np.log(h + epsilon) + (1 - y) * np.log(1 - h + epsilon)
+    )
     return cost
+
 
 def gradient_descent(X, y, weights, learning_rate, iterations):
     m = len(y)
     cost_history = []
-
 
     for i in range(iterations):
         h = sigmoid(np.dot(X, weights))
@@ -32,11 +36,8 @@ def gradient_descent(X, y, weights, learning_rate, iterations):
         weights -= learning_rate * gradient
         cost = compute_cost(X, y, weights)
         cost_history.append(cost)
-    
-    return weights, cost_history
 
-def transform_labels(y, class_label):
-    return np.where(y == class_label, 1, 0)
+    return weights, cost_history
 
 
 def transform_labels(y, class_label):
@@ -46,22 +47,42 @@ def transform_labels(y, class_label):
 def prepare_data(df, target_column):
     y = df[target_column]
     X = df.drop(target_column, axis=1)
-    X = X.drop("Care of Magical Creatures", axis=1)  # Supprimer la colonne "Care of Magical Creatures" (contient des valeurs manquantes
-    X = X.drop("Arithmancy", axis=1)  # Supprimer la colonne "Arithmancy" (contient des valeurs manquantes)
+    X = X.drop(
+        "Care of Magical Creatures", axis=1
+    )  # Supprimer la colonne "Care of Magical Creatures" (contient des valeurs manquantes
+    X = X.drop(
+        "Arithmancy", axis=1
+    )  # Supprimer la colonne "Arithmancy" (contient des valeurs manquantes)
+    X = X.drop("Astronomy", axis=1)
     X = X.select_dtypes(include="number")  # Garder seulement les colonnes numériques
     X = X.fillna(X.mean())  # Remplacer les valeurs manquantes par la moyenne
     X = (X - X.mean()) / X.std()  # Normalisation
     X = np.hstack((np.ones((X.shape[0], 1)), X))  # Ajout d'une colonne de biais
     return X, y
 
-def  parsing_args():
-    parser = argparse.ArgumentParser(description='Votre programme de régression logistique.')
-    parser.add_argument('fichier', type=str, help="Le chemin du fichier à analyser.\n\n Doit etre placer en premier")
-    parser.add_argument('-gradient', choices=['standard', 'stochastic'], help="Choisir le type de descente de gradient à utiliser.")
-    parser.add_argument('-show', nargs='+', help="Afficher l'historique des coûts pour les maisons spécifiées: Gryffindor, Hufflepuff, Ravenclaw, Slytherin.\n\nExemple:./logreg_train.py datasets/dataset_train.csv -show Gryffindor Hufflepuff Ravenclaw Slytherin")
+
+def parsing_args():
+    parser = argparse.ArgumentParser(
+        description="Votre programme de régression logistique."
+    )
+    parser.add_argument(
+        "fichier",
+        type=str,
+        help="Le chemin du fichier à analyser.\n\n Doit etre placer en premier",
+    )
+    parser.add_argument(
+        "-gradient",
+        choices=["standard", "stochastic"],
+        help="Choisir le type de descente de gradient à utiliser.",
+    )
+    parser.add_argument(
+        "-show",
+        nargs="+",
+        help="Afficher l'historique des coûts pour les maisons spécifiées: Gryffindor, Hufflepuff, Ravenclaw, Slytherin.\n\nExemple:./logreg_train.py datasets/dataset_train.csv -show Gryffindor Hufflepuff Ravenclaw Slytherin",
+    )
 
     args = parser.parse_args()
-    
+
     if args.show:
         # Afficher l'historique des coûts pour les maisons spécifiées
         tab = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
@@ -73,30 +94,30 @@ def  parsing_args():
                 print(f"La maison {element} n'existe pas")
                 print(f"Les maisons existantes sont: {tab}")
                 exit(1)
-        
+
         ## On enleve les doublons
         args.show = list(set(args.show))
-        
+
     if args.gradient:
         # Choisir le type de gradient
         print(f"Utilisation du gradient {args.gradient}")
-        
 
-    if (args.fichier):
+    if args.fichier:
         print(f"Le fichier à analyser est {args.fichier}")
-        if (args.fichier != "datasets/dataset_train.csv"):
+        if args.fichier != "datasets/dataset_train.csv":
             print(f"Le fichier à analyser doit être datasets/dataset_train.csv")
             exit(1)
- 
+
     return args
+
 
 def main():
 
-    args = parsing_args() 
+    args = parsing_args()
 
     df = pd.read_csv(args.fichier)
 
-    target_column = 'Hogwarts House'
+    target_column = "Hogwarts House"
     X, y = prepare_data(df, target_column)
 
     classes = y.unique()
@@ -106,31 +127,30 @@ def main():
     for class_label in classes:
         y_transformed = transform_labels(y, class_label)
         initial_weights = np.zeros(X.shape[1])
-        weights[class_label], _ = gradient_descent(X, y_transformed, initial_weights, learning_rate, iterations)
-        if (args.show is not None and class_label in args.show):
+        weights[class_label], _ = gradient_descent(
+            X, y_transformed, initial_weights, learning_rate, iterations
+        )
+        if args.show is not None and class_label in args.show:
             ## Print the cost history for Ravenclaw
             ## using graphique to see the cost history
             plt.plot(_)
-            plt.xlabel('Iterations')
-            plt.ylabel('Cost')
-            plt.title('Cost History for {}'.format(class_label))
+            plt.xlabel("Iterations")
+            plt.ylabel("Cost")
+            plt.title("Cost History for {}".format(class_label))
             plt.show()
 
-
-
     ## Sauvegarde des poids dans un fichier
-    np.save('weights.npy', weights)        
+    np.save("weights.npy", weights)
     exit(1)
 
 
-
 if __name__ == "__main__":
-##    try:
-        ##dataset_pathname = sys.argv[1]
+    ##    try:
+    ##dataset_pathname = sys.argv[1]
     ##except Exception:
-        ##print_usage()
-        ##exit(1)
-    try :
+    ##print_usage()
+    ##exit(1)
+    try:
         main()
     except Exception as e:
         print(e)
--- scatter_plot.py
+++ scatter_plot.py
@@ -16,7 +16,7 @@ def pearson_correlation(X, Y):
     mean_X = np.mean(X)
     mean_Y = np.mean(Y)
     numerator = np.sum((X - mean_X) * (Y - mean_Y))
-    denominator = np.sqrt(np.sum((X - mean_X)**2) * np.sum((Y - mean_Y)**2))
+    denominator = np.sqrt(np.sum((X - mean_X) ** 2) * np.sum((Y - mean_Y) ** 2))
     return numerator / denominator if denominator != 0 else 0
 
 
@@ -25,7 +25,7 @@ def compute_correlation_matrix(data):
     n = data.shape[1]  # Nombre de caractéristiques
     corr_matrix = np.zeros((n, n))  # Initialisation de la matrice de corrélation
     for i in range(n):
-        
+
         for j in range(i + 1, n):
             # Calculer seulement pour j > i
             corr = pearson_correlation(data.iloc[:, i], data.iloc[:, j])
@@ -37,18 +37,23 @@ def compute_correlation_matrix(data):
 
 def find_max_correlation(corr_matrix):
     """Trouve la paire de caractéristiques avec la corrélation maximale."""
-    max_corr_value = 1  # Initialiser avec une valeur impossible pour une corrélation valide
+    max_corr_value = (
+        1  # Initialiser avec une valeur impossible pour une corrélation valide
+    )
     max_indices = (0, 0)  # Initialiser les indices de la paire maximale
     # Parcourir la matrice de corrélation
     for i in range(corr_matrix.shape[0]):
-        for j in range(i + 1, corr_matrix.shape[1]):  # Eviter la diagonale et les duplicatas
+        for j in range(
+            i + 1, corr_matrix.shape[1]
+        ):  # Eviter la diagonale et les duplicatas
             corr_distance = abs(abs(corr_matrix[i, j]) - 1)
-            if corr_distance < max_corr_value :
-                max_corr_value = abs(abs(corr_matrix[i, j]) -1)
+            if corr_distance < max_corr_value:
+                max_corr_value = abs(abs(corr_matrix[i, j]) - 1)
                 max_indices = (i, j)
-    
+
     print(f"correlation value: {corr_matrix[max_indices[0], max_indices[1]]}")
     return max_indices, max_corr_value
+
 
 def print_graph(data, a, b):
     selections = data["Hogwarts House"].unique()
@@ -58,13 +63,19 @@ def print_graph(data, a, b):
 
     plt.figure(figsize=(10, 6))
     for selection in selections:
-        plt.scatter(data[data["Hogwarts House"] == selection][var1], data[data["Hogwarts House"] == selection][var2], alpha=0.5, label=selection)
+        plt.scatter(
+            data[data["Hogwarts House"] == selection][var1],
+            data[data["Hogwarts House"] == selection][var2],
+            alpha=0.5,
+            label=selection,
+        )
     ##plt.scatter(data[var1], data[var2], alpha=0.5)
     plt.title(f"Scatter Plot entre {var1} et {var2}")
     plt.xlabel(var1)
     plt.ylabel(var2)
     plt.grid(True)
     plt.show()
+
 
 if __name__ == "__main__":
     try:
@@ -77,12 +88,12 @@ if __name__ == "__main__":
     X = df["Hogwarts House"]
     df = df.select_dtypes(include="number")
 
-
     ## Data clean, ready to get exploited
-
 
     corr = compute_correlation_matrix(df)
     a, b = find_max_correlation(corr)
-    print(f"La paire de caractéristiques avec la corrélation maximale est: {df.columns[a[0]]} et {df.columns[a[1]]}")
+    print(
+        f"La paire de caractéristiques avec la corrélation maximale est: {df.columns[a[0]]} et {df.columns[a[1]]}"
+    )
     df["Hogwarts House"] = X
     print_graph(df, a[0], a[1])
